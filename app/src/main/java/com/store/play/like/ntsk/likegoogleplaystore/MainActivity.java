4f2a86df0eb6cedb2987df14a6591a5221ef26ba
ntsk <ridha.h2a26@gmail.com>
2017-10-01 20:42:39 +0900

    Inital commit

---
+++ app/src/main/java/com/store/play/like/ntsk/likegoogleplaystore/MainActivity.java
@@ -0,0 +1,13 @@
+package com.store.play.like.ntsk.likegoogleplaystore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
